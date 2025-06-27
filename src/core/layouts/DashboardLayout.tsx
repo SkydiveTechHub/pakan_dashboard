58bbe92eb90180b0148c7338d0b7c585d5428a94
Femi Akerele <lovinamercie@gmail.com>
2025-06-27 09:16:05 +0100

    updated

--- src/core/layouts/DashboardLayout.tsx
+++ src/core/layouts/DashboardLayout.tsx
@@ -14,7 +14,7 @@ const DashboardLayout = () => {
             <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
             <div className={`relative flex-1 transition-all duration-300 ml-0 lg:ml-48  overflow-x-hidden h-screen bg-[f4f4f4] `}>
               <Header toggleSidebar={toggleSidebar}/>
-              <div className="py-6 overflow-y-auto overflow-x-hidden mt-11 relative pb-12 px-8">
+              <div className="py-6 overflow-y-auto overflow-x-hidden mt-11 relative pb-12 px-2 md:px-8">
                 <Outlet />
               </div>
               {/* <TabBar/> */}
